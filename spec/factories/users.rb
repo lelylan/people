bc4e4dff23da242b46a5fd42cd598bb5f542735b
Andrea Reginato <andrea.reginato@gmail.com>
2012-07-19 13:00:34 +0200

    Added basic tests on login and logout

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :user do
+    email    'alice@example.com'
+    password 'alice'
   end
 end
--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,4 +1,70 @@
 require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
 
-feature "UserController" do
+describe '/signin' do
+
+  let!(:user) { FactoryGirl.create(:user) }
+  before      { visit('/users/sign_in')   }
+
+  it 'shows the sign in page' do
+    page.should have_content('Sign in')
+  end
+
+  context 'with valid email and password' do
+
+    before do
+      fill_in  'Email',    with: 'alice@example.com'
+      fill_in  'Password', with: 'alice'
+      click_on 'Sign in'
+    end
+
+    it 'signs in' do
+      page.should have_content('Signed in successfully.')
+    end
+  end
+
+  context 'with not valid email' do
+
+    before do
+      fill_in  'Email',    with: 'invalid@example.com'
+      fill_in  'Password', with: 'alice'
+      click_on 'Sign in'
+    end
+
+    it 'does not sign in' do
+      page.should have_content('Invalid email or password.')
+    end
+  end
+
+  context 'with not valid password' do
+
+    before do
+      fill_in  'Email',    with: 'alice@example.com'
+      fill_in  'Password', with: 'invalid'
+      click_on 'Sign in'
+    end
+
+    it 'does not sign in' do
+      page.should have_content('Invalid email or password.')
+    end
+  end
+end
+
+describe '/signout' do
+
+  let!(:user) { FactoryGirl.create(:user) }
+  before      { visit('/users/sign_in') }
+
+  context 'when logged in' do
+
+    before do
+      fill_in  'Email',    with: 'alice@example.com'
+      fill_in  'Password', with: 'alice'
+      click_on 'Sign in'
+      click_on 'Sign out'
+    end
+
+    it 'signs out' do
+      page.should have_content('Signed out successfully.')
+    end
+  end
 end
