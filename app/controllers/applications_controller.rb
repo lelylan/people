310869e907a310838104429ce89972f7645e4d08
Andrea Reginato <andrea.reginato@gmail.com>
2012-07-26 15:39:29 +0200

    Reformatted applications controller as it was one tab on right

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,58 +1,58 @@
-  class ApplicationsController < Doorkeeper::ApplicationController
-    respond_to :html
+class ApplicationsController < Doorkeeper::ApplicationController
+  respond_to :html
 
-    before_filter :authenticate_resource_owner!
-    before_filter :find_applications
+  before_filter :authenticate_resource_owner!
+  before_filter :find_applications
 
-    def index
-    end
+  def index
+  end
 
-    def new
-      @application = Doorkeeper::Application.new
-    end
+  def new
+    @application = Doorkeeper::Application.new
+  end
 
-    def create
-      @application = Doorkeeper::Application.new(params[:application])
-      @application.resource_owner_id = current_resource_owner.id
-      if @application.save
-        flash[:notice] = "Application created"
-        respond_with [:oauth, @application]
-      else
-        render :new
-      end
-    end
-
-    def show
-      @application = @applications.find(params[:id])
-    end
-
-    def edit
-      @application = @applications.find(params[:id])
-    end
-
-    def update
-      @application = @applications.find(params[:id])
-      if @application.update_attributes(params[:application])
-        flash[:notice] = "Application updated"
-        respond_with [:oauth, @application]
-      else
-        render :edit
-      end
-    end
-
-    def destroy
-      @application = @applications.find(params[:id])
-      flash[:notice] = "Application deleted" if @application.destroy
-      redirect_to oauth_applications_url
-    end
-
-    private
-
-    def find_applications
-      @applications = if current_resource_owner.admin?
-        Doorkeeper::Application.all
-      else
-        Doorkeeper::Application.where(resource_owner_id: current_resource_owner.id)
-      end
+  def create
+    @application = Doorkeeper::Application.new(params[:application])
+    @application.resource_owner_id = current_resource_owner.id
+    if @application.save
+      flash[:notice] = "Application created"
+      respond_with [:oauth, @application]
+    else
+      render :new
     end
   end
+
+  def show
+    @application = @applications.find(params[:id])
+  end
+
+  def edit
+    @application = @applications.find(params[:id])
+  end
+
+  def update
+    @application = @applications.find(params[:id])
+    if @application.update_attributes(params[:application])
+      flash[:notice] = "Application updated"
+      respond_with [:oauth, @application]
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @application = @applications.find(params[:id])
+    flash[:notice] = "Application deleted" if @application.destroy
+    redirect_to oauth_applications_url
+  end
+
+  private
+
+  def find_applications
+    @applications = if current_resource_owner.admin?
+                      Doorkeeper::Application.all
+                    else
+                      Doorkeeper::Application.where(resource_owner_id: current_resource_owner.id)
+                    end
+  end
+end
