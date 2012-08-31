630e7e2ca95d18c5dd874810e41933909019534b
Andrea Reginato <andrea.reginato@gmail.com>
2012-08-31 12:20:48 +0200

    Uniformed message errors handling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,4 +29,22 @@ module ApplicationHelper
       state:         'remember-me' }
     oauth_authorization_path(authorization_params)
   end
+
+  def lelylan_error_messages_for(resource)
+    return '' if resource.errors.empty?
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t('errors.messages.not_saved',
+                      :count => resource.errors.count,
+                      :resource => resource.class.model_name.human.downcase)
+    html = <<-HTML
+    <div class="errors">
+      <div id="error_explanation">
+        <h2>#{sentence}</h2>
+        <ul>#{messages}</ul>
+      </div>
+    </div>
+    HTML
+
+    html.html_safe
+  end
 end
