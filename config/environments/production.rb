1d76cab198d774fe440eead8119a7e8e6ff069e4
Andrea Reginato <andrea.reginato@gmail.com>
2012-12-19 20:21:45 +0100

    Minor changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,6 @@ People::Application.configure do
   # enable the same cookies for authenticating diffferent domains
   config.session_store :cookie_store, key: '_people_session', domain: ENV['SESSION_STORE_DOMAIN']
 
-  # CDN settings
+  # CDN configuration
   config.action_controller.asset_host = "//#{ENV['FOG_CLOUDFRONT']}.cloudfront.net"
 end
